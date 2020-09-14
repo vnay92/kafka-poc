c7d870e4305536a85aaaf5a435da771830c19317
Vinay Bharadwaj <vinay.bharadwaj@matchmove.com>
2020-09-14 18:23:18 +0530

    Added Kafka UI

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Kafka } = require('kafkajs')
 
 const kafka = new Kafka({
     clientId: 'my-app',
-    brokers: ['kafka:9092']
+    brokers: ['kafka:29092']
 });
 
 const produce = async () => {
